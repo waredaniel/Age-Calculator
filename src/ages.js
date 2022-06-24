66693368d6c857d99ed305e1f1ea7debf46e7244
danielware <waredanielb@gmail.com>
2022-06-24 12:41:32 -0700

    Pass fourth test.

--- src/ages.js
+++ src/ages.js
@@ -3,5 +3,6 @@ export class PersonAge {
     this.earthAge = earthAge;
     this.mercuryAge = Math.round(this.earthAge / 0.24);
     this.venusAge = Math.round(this.earthAge/ 0.62);
+    this.marsAge = Math.round(this.earthAge/ 1.88);
   }
 }
