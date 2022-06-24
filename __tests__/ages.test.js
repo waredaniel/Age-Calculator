14d43fd53a047325848bb0523bf74b4d5a816ed0
danielware <waredanielb@gmail.com>
2022-06-24 08:54:04 -0700

    Create first test.

---
+++ __tests__/ages.test.js
@@ -0,0 +1,8 @@
+import Ages from '../src/index.js';
+
+describe ('Ages'), () => {
+  test('should create a subject object with earth age property', ()=> {
+    expect(person.earthage).toEqual(35);
+  }
+
+)};
